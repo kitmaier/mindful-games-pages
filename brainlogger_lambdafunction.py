4dbeb1ee4b8e2240d892b65c9d0ca3911276bd56
Kit Maier <8maier@gmail.com>
2019-12-09 18:28:16 -0800

    brainlogger - create backend service to store notes (part 3)

--- brainlogger_lambdafunction.py
+++ brainlogger_lambdafunction.py
@@ -27,3 +27,13 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*' 
         }
     }
+
+# Notes on how to set up this function:
+# In S3 create a bucket and folder in the Oregon region with the chosen names and public access
+# In Lambda create a new function in the Oregon region as "Author from scratch" with the most recent Python runtime
+# In the configuration designer hit "Add trigger", select "API Gateway", "Create a new API", "REST API", Open security
+# Grab the url of the API endpoint, this is what the browser will need to hit
+# In the configuration designer select the lambda function and copy the above code into the editor and hit Save
+# In the configuration designer in the Execution Role section click on "View the ----- role in the IAM console" link
+# Click "Attach policies" search for S3 and check the box next to AmazonS3FullAccess and hit "Attach policy"
+# In the configuration designer hit Test, create a new dummy test, and then run it
