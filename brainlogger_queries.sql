c0e3c35f4cce3a32343e9a032c9a7c21a6a8262f
Kit Maier <8maier@gmail.com>
2019-12-09 18:19:01 -0800

    brainlogger - create backend service to store notes (part 2)

--- brainlogger_queries.sql
+++ brainlogger_queries.sql
@@ -19,3 +19,24 @@ LOCATION 's3://generic-use/brainlogger/48895411585313275/';
 /* select all data for a session */
 
 select * from table48895411585313275 order by cast(sequence as bigint);
+
+/* create table in athena to support queries on s3 for all sessions */
+
+CREATE EXTERNAL TABLE IF NOT EXISTS brainloggertable (
+  game string,
+  ip string,
+  session string,
+  sequence string,
+  timestamp string,
+  recorddate string,
+  startquiet string,
+  startnote string,
+  endnote string,
+  notetext string
+) 
+ROW FORMAT serde 'org.apache.hive.hcatalog.data.JsonSerDe'
+LOCATION 's3://generic-use/brainlogger/';
+
+/* select all data for a session */
+
+select * from brainloggertable order by cast(timestamp as bigint);
